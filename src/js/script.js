c273dcb4ea685c304f0247b11b07b7b6024c12cc
Adriel Modollo <adriel.modollo@lit.com.br>
2025-03-24 16:14:28 -0300

    feat: solar-borges

---
+++ src/js/script.js
@@ -0,0 +1,23 @@
+document.getElementById('orcamentoForm').addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const dados = {
+        titulo: encodeURIComponent("📊 NOVO ORÇAMENTO DE ENERGIA SOLAR"),
+        nome: encodeURIComponent(document.getElementById('nome').value),
+        email: encodeURIComponent(document.getElementById('email').value),
+        cidade: encodeURIComponent(document.getElementById('cidade').value),
+        info: encodeURIComponent(document.getElementById('informacoes').value)
+    };
+
+    const whatsappLink =
+        `https://api.whatsapp.com/send/?phone=5516981790555` +
+        `&text=` +
+        `*${dados.titulo}*%0A%0A` +
+        `*▪ Nome:* ${dados.nome}%0A` +
+        `*▪ E-mail:* ${dados.email}%0A` +
+        `*▪ Cidade:* ${dados.cidade}%0A` +
+        `*▪ Informações:* ${dados.info}%0A%0A` +
+        encodeURIComponent("📎 Anexe sua conta de luz");
+
+    window.open(whatsappLink, '_blank');
+});
